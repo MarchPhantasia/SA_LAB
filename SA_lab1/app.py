0b417b1bae49304f40ebe79307f6b94991e72263
Phantasia <1162030037@qq.com>
2024-10-26 23:40:19 +0800

    hadoop完成、文件提交

--- SA_lab1/app.py
+++ SA_lab1/app.py
@@ -36,7 +36,7 @@ def get_history():
 
 def publish_message(platform, message):
     """通过 HTTP 请求将消息发布到 Broker。"""
-    url = "http://192.168.2.12:11454/publish"
+    url = "http://192.168.65.22:11454/publish"
     print(message)
     payload = {
         "platform": platform,
